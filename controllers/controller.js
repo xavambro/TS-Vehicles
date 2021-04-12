f22645015ef98cb82f6c7f21a6f5d2814d10e6bd
xavambro <xavambro@gmail.com>
2021-04-12 18:27:17 +0200

    Nivel 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,8 +1,31 @@
 "use strict";
+var formCar = document.forms[0];
+var formWheels = document.forms[1];
+var carInfo = document.getElementById('carInfo');
+var car;
 function createCar(plate, brand, color) {
-    var car = new Car(plate, color, brand);
-    car.addWheel(new Wheel(2, "SEAT"));
-    document.body.innerText = "CAR: PLATE: " + car.plate
-        + " COLOR: " + car.color + " BRAND: " + brand
+    car = new Car(plate, color, brand);
+    formCar.className = "d-none";
+    formWheels.className = "";
+    /* car.addWheel(new Wheel(2,"SEAT"));
+    carInfo.textContent="CAR: PLATE: " + car.plate
+    + " COLOR: " +car.color + " BRAND: " + brand
+    + " WHEELS: " + JSON.stringify(car.wheels); */
+}
+function createWheels(wheel1b, wheel1d, wheel2b, wheel2d, wheel3b, wheel3d, wheel4b, wheel4d) {
+    var wheels = [];
+    var wheel1 = new Wheel(wheel1d, wheel1b);
+    wheels.push(wheel1);
+    var wheel2 = new Wheel(wheel2d, wheel2b);
+    wheels.push(wheel2);
+    var wheel3 = new Wheel(wheel3d, wheel3b);
+    wheels.push(wheel3);
+    var wheel4 = new Wheel(wheel4d, wheel4b);
+    wheels.push(wheel4);
+    for (var i = 0; i < wheels.length; i++) {
+        car.addWheel(wheels[i]);
+    }
+    carInfo.textContent = "CAR: PLATE: " + car.plate
+        + " COLOR: " + car.color + " BRAND: " + car.brand
         + " WHEELS: " + JSON.stringify(car.wheels);
 }
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,9 +1,36 @@
+let formCar: HTMLFormElement = document.forms[0];
+let formWheels: HTMLFormElement = document.forms[1];
+let carInfo:any = document.getElementById('carInfo');
+let car:Car
 
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    car.addWheel(new Wheel(2,"SEAT"));
-    document.body.innerText="CAR: PLATE: " + car.plate 
+function createCar(plate:string,brand:string,color:string):void {
+    
+    car=new Car(plate,color,brand);
+    formCar.className = "d-none";
+    formWheels.className = "";
+    /* car.addWheel(new Wheel(2,"SEAT"));
+    carInfo.textContent="CAR: PLATE: " + car.plate 
     + " COLOR: " +car.color + " BRAND: " + brand 
-    + " WHEELS: " + JSON.stringify(car.wheels);
+    + " WHEELS: " + JSON.stringify(car.wheels); */
 }
+function createWheels(wheel1b:string,wheel1d:number,wheel2b:string,wheel2d:number,wheel3b:string,wheel3d:number,wheel4b:string,wheel4d:number):void {
+    let wheels: Wheel[] = [];
+    let wheel1: Wheel = new Wheel(wheel1d,wheel1b);
+    wheels.push(wheel1);
+    let wheel2: Wheel = new Wheel(wheel2d,wheel2b);
+    wheels.push(wheel2);
+    let wheel3: Wheel = new Wheel(wheel3d,wheel3b);
+    wheels.push(wheel3);
+    let wheel4: Wheel = new Wheel(wheel4d,wheel4b);
+    wheels.push(wheel4);
+    for (let i:number = 0;i < wheels.length;i++){
+        car.addWheel(wheels[i]);
+    }
+
+    carInfo.textContent="CAR: PLATE: " + car.plate 
+    + " COLOR: " +car.color + " BRAND: " + car.brand 
+    + " WHEELS: " + JSON.stringify(car.wheels); 
+    
+}
+
 
